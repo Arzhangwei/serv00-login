b25ba6f38a6d71d512de9b8eb5e984c06958dbe2
Arzhangwei <157704180+Arzhangwei@users.noreply.github.com>
2024-07-22 16:58:03 +0800

    Add files via upload

--- login_script.py
+++ login_script.py
@@ -6,15 +6,45 @@ import aiofiles
 import random
 import requests
 import os
+import paramiko
+import datetime
 
 # 从环境变量中获取 Telegram Bot Token 和 Chat ID
+ss = requests.session()
 PUSHPLUS_TOKEN = os.getenv('PUSHPLUS_TOKEN')
-pushplus = f'http://www.pushplus.plus/send?token={PUSHPLUS_TOKEN}&title=Serv00自动续期&content='
 
-async def push_wx(warnInfo):
-    response = requests.get(f"{pushplus}{warnInfo}",verify=False)
-    if "code\": \"0" in response.text:
-        pass
+async def pushWX(conten):
+    result = ss.get(f"https://wxpusher.zjiecode.com/demo/send/custom/{PUSHPLUS_TOKEN}?content={conten}").json()
+    if result['code'] == 1000:
+        print(f"账号Wxpusher 通知: 推送成功!")
+    else:
+        print(f"账号Wxpusher 通知: 推送失败!")
+
+
+#####################函数定义start######################
+# 
+#ssh链接主机并执行命令，返回结果
+#
+async def ssh_with_key(hostname, userName, command,passWord):
+    try:
+        ssh_client = paramiko.SSHClient()
+        
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # 连接交换机
+        ssh_client.connect(hostname, username=userName, password=passWord)
+        #print("登陆成功")
+        # 执行 portcfgshow 命令
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+        output = stdout.read().decode()  # 将字节数据解码为字符串
+        #print(f"ssh_with_key 返回的是{output}")
+        return output 
+        
+    except Exception as e:
+        print(f"ssh_with_key_FUNC An error occurred: {e}")
+    finally:
+        # 关闭SSH连接
+        ssh_client.close()
+
 
 def format_to_iso(date):
     return date.strftime('%Y-%m-%d %H:%M:%S')
@@ -38,7 +68,7 @@ async def login(username, password, panel):
             browser = await launch(headless=True, args=['--no-sandbox', '--disable-setuid-sandbox'])
 
         page = await browser.newPage()
-        url = f'https://panel{panel}.serv00.com/login/?next=/'
+        url = f'https://{panel}/login/?next=/'
         await page.goto(url)
 
         username_input = await page.querySelector('#id_username')
@@ -87,6 +117,7 @@ async def main():
         username = account['username']
         password = account['password']
         panel = account['panelnum']
+        sshIPaddress = account['sshIPaddress']
 
         serviceName = 'ct8' if 'ct8' in panel else 'serv00'
         is_logged_in = await login(username, password, panel)
@@ -97,7 +128,7 @@ async def main():
             success_message = f'{serviceName}账号 {username} 于北京时间 {now_beijing}（UTC时间 {now_utc}）登录成功！'
             message += success_message + '\n'
             print(success_message)
-            await push_wx(success_message)
+            await pushWX(success_message)
         else:
             message += f'{serviceName}账号 {username} 登录失败，请检查{serviceName}账号和密码是否正确。\n'
             print(f'{serviceName}账号 {username} 登录失败，请检查{serviceName}账号和密码是否正确。')
@@ -109,6 +140,10 @@ async def main():
     
     print(f'所有{serviceName}账号登录完成！')
 
+    stdout_result = await ssh_with_key(sshIPaddress,username,"sh /usr/home/bankwjj/check_xray.sh",password)
+    print(stdout_result)
+    if 'xray进程正在运行' in stdout_result:
+        print('xray进程正在运行')
 
 
 if __name__ == '__main__':
